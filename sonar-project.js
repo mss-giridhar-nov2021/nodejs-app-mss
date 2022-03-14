e93bf3b2065a46f0d4f650b0fe37687e5ee4f50a
giribabu1500 <96191501+giribabu1500@users.noreply.github.com>
2022-03-14 11:24:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.8.220:9000/',
+  serverUrl: 'http://18.212.173.212:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '2da81d6a48103676b04da8faaf136fc7d8ca231a',
+	    'sonar.login': '3a63b24599bc4eb1c705d6dcb0b37834128f1af8',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
